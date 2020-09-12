202f933dbe1846294e0d2932de17b6b87fa0ca4b
Anthony Hernandez <anthonyh2020@gmail.com>
2020-09-12 05:15:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,36 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :author, :page_count, :genre
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count
+    @page_count
+  end 
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
+
