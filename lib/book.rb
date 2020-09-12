8759342f199c7b9a864b05a5663ca3cf32a76ed0
Anthony Hernandez <anthonyh2020@gmail.com>
2020-09-12 05:08:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,30 @@
 class Book
   def initialize(title)
-end
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count
+    @page_count
+  end 
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
 end
 
